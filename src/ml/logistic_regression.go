7975721eacf9fa3bfb5f01530fff6dfe628af67e
phanuruj <phuphanuruj@gmail.com>
2021-06-30 18:09:09 +0700

    Create logistic_regression.go (#2)

---
+++ src/ml/logistic_regression.go
@@ -0,0 +1,10 @@
+package ml
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/ldsec/lattigo/v2/ckks"
+	"github.com/perm-ai/GO-HEML-prototype/src/logger"
+	"github.com/perm-ai/GO-HEML-prototype/src/utility"
+)
