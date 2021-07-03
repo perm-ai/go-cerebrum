daca670f89cdab1200c7eeb8d354c558eaefcf60
phanuruj <phuphanuruj@gmail.com>
2021-07-03 10:53:17 +0700

    Create importerLogistic.go (#2)
    Copied and edited original importer.go because an additional column is needed in classification tasks.

---
+++ src/importer/importerLogistic.go
@@ -0,0 +1,32 @@
+package importer
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+)
+
+type HeartData struct {
+
+	// Not sure if we should be using Age & Sex as float because they are all unsighned ints
+	// (not sure if it'll mess something up in the process of using ckks scheme)
+	// Basic Logistic Regression is basically evualating linear regression then inputting the data
+	// into Sigmoid which will turn the output into a probability (0,1) in which outputs >0.5 will
+	// result in class A and <0.5 in class B (classification between A and B)
+
+	Age []float64
+	Sex  []float64
+	Target []uint8
+
+}
+
+func GetHeartData(filepath string) HeartData {
+	jsonFile, _ := os.Open(filepath)
+	defer jsonFile.Close()
+	file, _ := ioutil.ReadAll(jsonFile)
+
+	var data HeartData
+	json.Unmarshal(file, &data)
+
+	return data
+}
