d82c1d1d1567a68641a40a26d3a1a46c6c91b68b
phanuruj <phuphanuruj@gmail.com>
2021-07-05 17:53:32 +0700

    Comment out all Logistic Regression (#2)

--- src/ml/logistic_regression.go
+++ src/ml/logistic_regression.go
@@ -1,48 +1,48 @@
 package ml
 
-import (
-	"fmt"
-	"strconv"
+// import (
+// 	// "fmt"
+// 	// "strconv"
 
-	"github.com/ldsec/lattigo/v2/ckks"
-	"github.com/perm-ai/GO-HEML-prototype/src/logger"
-	"github.com/perm-ai/GO-HEML-prototype/src/utility"
-)
+// 	"github.com/ldsec/lattigo/v2/ckks"
+// 	// "github.com/perm-ai/GO-HEML-prototype/src/logger"
+// 	// "github.com/perm-ai/GO-HEML-prototype/src/utility"
+// )
 
-type LogisticRegression struct {
-	utils utility.Utils
-	b0     ckks.Ciphertext
-	b1     ckks.Ciphertext
-	b2     ckks.Ciphertext
-}
+// type LogisticRegression struct {
+// 	utils utility.Utils
+// 	b0     ckks.Ciphertext
+// 	b1     ckks.Ciphertext
+// 	b2     ckks.Ciphertext
+// }
 
-type LogisticRegressionGradient struct {
-	Db0 ckks.Ciphertext
-	Db1 ckks.Ciphertext
-	Db2 ckks.Ciphertext
-}
+// type LogisticRegressionGradient struct {
+// 	Db0 ckks.Ciphertext
+// 	Db1 ckks.Ciphertext
+// 	Db2 ckks.Ciphertext
+// }
 
-func NewLogisticRegression(u utility.Utils) LogisticRegression {
+// func NewLogisticRegression(u utility.Utils) LogisticRegression {
 
-	zeros := u.GenerateFilledArray(0.0)
-	b0 := u.Encrypt(zeros)
-	b1 := u.Encrypt(zeros)
-	b2 := u.Encrypt(zeros)
+// 	zeros := u.GenerateFilledArray(0.0)
+// 	b0 := u.Encrypt(zeros)
+// 	b1 := u.Encrypt(zeros)
+// 	b2 := u.Encrypt(zeros)
 
-	return LogisticRegression{u, b0, b1, b2}
+// 	return LogisticRegression{u, b0, b1, b2}
 
-}
+// }
 
-func Evaluate(b0 ckks.Ciphertext, b1 ckks.Ciphertext, b2 ckks.Ciphertext, ) {
+// func Evaluate(b0 ckks.Ciphertext, b1 ckks.Ciphertext, b2 ckks.Ciphertext, ) {
 
-}
+// }
 
-func Sigmoid(x ckks.Ciphertext) ckks.Ciphertext {
+// func Sigmoid(x ckks.Ciphertext) ckks.Ciphertext {
 
 
-}
+// }
 
-func (model *LogisticRegression) Train(x *ckks.Ciphertext, y *ckks.Ciphertext, target int, learningRate float64, size int, epoch int) {
+// func (model *LogisticRegression) Train(x *ckks.Ciphertext, y *ckks.Ciphertext, target int, learningRate float64, size int, epoch int) {
 
-}
+// }
 
