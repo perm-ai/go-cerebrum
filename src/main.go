4f617f43c9262007250d1664e66de44a3945a1d8
phanuruj <phuphanuruj@gmail.com>
2021-07-06 20:18:52 +0700

    Test mult and addition

--- src/main.go
+++ src/main.go
@@ -17,14 +17,14 @@ import (
 func main() {
 
 
-	lrData := importer.GetTitanicData("./test-data/titanic1.json")
-	x := lrData.Age
-	y := lrData.Pclass
-	target := lrData.Target
-	ml.Normalize_Data(x)
-	ml.Normalize_Data(y)
-	logisticRegression := ml.NewLogisticRegression()
-	ml.Train(logisticRegression, x, y, target, 0.1, 20)
+	// lrData := importer.GetTitanicData("./test-data/titanic1.json")
+	// x := lrData.Age
+	// y := lrData.Pclass
+	// target := lrData.Target
+	// ml.Normalize_Data(x)
+	// ml.Normalize_Data(y)
+	// logisticRegression := ml.NewLogisticRegression()
+	// ml.Train(logisticRegression, x, y, target, 0.1, 20)
 
 
 	// Acc := 0.0
@@ -34,5 +34,23 @@ func main() {
 	// 	Acc += ml.Train(logisticRegression, x, y, target, 0.1, 20)
 	// }
 	// fmt.Printf("Average Accuracy : %f", Acc/10)
+
+	utils := utility.NewUtils(math.Pow(2, 35), 0, true, true)
+	testArray := utils.GenerateRandomNormalArray(5)
+
+	for i := 0; i < 5; i++ {
+		fmt.Println(testArray[i])
+	}
+
+	encryptedTestArray := utils.Encrypt(testArray)
+	// testCipher := utils.MultiplyConstNew(encryptedTestArray, utils.GenerateFilledArraySize(2, 10),true ,false)
+
+	// testCipher := ml.Sigmoid(encryptedTestArray)
+
+	testcipher := utils.AddNew(encryptedTestArray, encryptedTestArray)
+	decryptedTestArray := utils.Decrypt(&testCipher)
+
+	for i := 0; i < 5; i++ {
+		fmt.Println(decryptedTestArray[i])
 
 }
