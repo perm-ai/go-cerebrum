c5e7f19b83d4b6594fc9376d0cf920896693acca
phanuruj <phuphanuruj@gmail.com>
2021-08-04 20:26:35 +0700

    update main

--- main.go
+++ main.go
@@ -21,9 +21,10 @@ func main() {
 		data[i][1] = data2[i]
 		data[i][2] = data3[i]
 		data[i][3] = 1
-
 	}
+	fmt.Printf("This dataset consists of %d data points and %d features \n", len(data), len(data[0]))
+	fmt.Printf("Target has %d data points\n", len(target))
 	model := svm.NewSVMModel()
-	weights := model.TrainSVM(data, target, 20, 0.01, 3, 1000)
+	weights := model.TrainSVM(data, target, 20, 0.001, 4, 10000)
 	fmt.Printf("The weights of the model are %f \n", weights)
 }
