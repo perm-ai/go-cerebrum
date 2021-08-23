95ee02c8bc4c1587f2c75485c47c380b2d90c55e
FongChanatip <fongchanatip@gmail.com>
2021-08-23 21:05:31 +0700

    Create batch version of dense layer

--- layers/dense.go
+++ layers/dense.go
@@ -3,41 +3,18 @@ package layers
 import (
 	"github.com/ldsec/lattigo/v2/ckks"
 	"github.com/perm-ai/go-cerebrum/activations"
+	"github.com/perm-ai/go-cerebrum/array"
 	"github.com/perm-ai/go-cerebrum/utility"
 )
 
 //=================================================
-//			  NEURAL NETWORK GRADIENT
+//			  DENSE GRADIENT
 //=================================================
 
-type NeuralNetworkGradient struct {
-	BiasGradient   ckks.Ciphertext
-	WeightGradient []ckks.Ciphertext
-}
-
-func (n NeuralNetworkGradient) GroupGradients(utils utility.Utils, weightLength int, biasLenght int) utility.CiphertextGroup {
-
-	ctData := make([]utility.CiphertextData, len(n.WeightGradient)+1)
-
-	for i := range n.WeightGradient {
-
-		ctData[i] = utility.CiphertextData{Ciphertext: n.WeightGradient[i], Length: weightLength}
-
-	}
-
-	ctData[len(n.WeightGradient)] = utility.CiphertextData{Ciphertext: n.BiasGradient, Length: biasLenght}
-
-	return utility.NewCiphertextGroup(ctData, utils)
-
-}
-
-func (n *NeuralNetworkGradient) LoadFromGroup(group utility.CiphertextGroup, rescale bool) {
-
-	ct := group.BreakGroup(rescale)
-
-	n.WeightGradient = ct[0 : len(ct)-1]
-	n.BiasGradient = ct[len(ct)-1]
-
+type DenseGradient struct {
+	BiasGradient   		[]*ckks.Ciphertext
+	WeightGradient 		[][]*ckks.Ciphertext
+	PrevLayerGradient	[]*ckks.Ciphertext
 }
 
 //=================================================
@@ -45,129 +22,115 @@ func (n *NeuralNetworkGradient) LoadFromGroup(group utility.CiphertextGroup, res
 //=================================================
 
 type Dense struct {
+
+	utils         utility.Utils
 	InputUnit     int
 	OutputUnit    int
-	Weights       []ckks.Ciphertext
-	Bias          ckks.Ciphertext
-	Activation    activations.Activation
-	UseActivation bool
-	utils         utility.Utils
-
-	Declared bool
-}
-
-func NewDense(inputUnit int, outputUnit int, activation activations.Activation, useActivation bool, utils utility.Utils) Dense {
-
-	weights := make([]ckks.Ciphertext, outputUnit)
-
-	for i := range weights {
-
-		weights[i] = utils.Encrypt(utils.GenerateRandomNormalArray(inputUnit))
-
-	}
-
-	bias := utils.Encrypt(utils.GenerateRandomNormalArray(outputUnit))
-
-	return Dense{inputUnit, outputUnit, weights, bias, activation, useActivation, utils, true}
+	Weights       [][]*ckks.Ciphertext
+	Bias          []*ckks.Ciphertext
+	Activation    *activations.Activation
 
 }
 
-func (model Dense) Forward(input *ckks.Ciphertext) (ckks.Ciphertext, ckks.Ciphertext) {
+func NewDense(utils utility.Utils, inputUnit int, outputUnit int, activation *activations.Activation, useBias bool) Dense {
 
-	results := make([]ckks.Ciphertext, model.OutputUnit)
+	// Generate random weights and biases
+	weights := make([][]*ckks.Ciphertext, outputUnit)
+	bias := make([]*ckks.Ciphertext, outputUnit)
 
-	// Loop through each node in the layer and apply it to the data
-	for node := 0; node < model.OutputUnit; node++ {
+	randomBias := array.GenerateRandomNormalArray(outputUnit)
 
-		results[node] = model.utils.DotProductNew(*input, model.Weights[node], false)
+	for node := 0; node < outputUnit; node++ {
 
-	}
+		randomWeight := array.GenerateRandomNormalArray(inputUnit)
+		weights[node] = make([]*ckks.Ciphertext, inputUnit)
 
-	z := model.utils.PackVector(results)
-	model.utils.Add(z, model.Bias, &z)
+		if useBias {
+			bias[node] = utils.EncryptToPointer(utils.GenerateFilledArray(randomBias[node]))
+		}
 
-	if model.UseActivation {
-		a := model.Activation.Forward(z, model.OutputUnit)
-		return z, a
-	} else {
-		return z, z
-	}
+		for weight := 0; weight < inputUnit; weight++ {
 
-}
-
-func (model Dense) Backward(input *ckks.Ciphertext, output *ckks.Ciphertext, gradient ckks.Ciphertext, nextLayerTransposedWeight []ckks.Ciphertext, lr float64) NeuralNetworkGradient {
-
-	gradients := NeuralNetworkGradient{}
-
-	if len(nextLayerTransposedWeight) == 0 {
-
-		if model.UseActivation {
-
-			activationGradient := model.Activation.Backward(*output, model.OutputUnit)
-			gradients.BiasGradient = model.utils.MultiplyNew(gradient, activationGradient, true, false)
-
-		} else {
-
-			gradients.BiasGradient = gradient
+			weights[node][weight] = utils.EncryptToPointer(utils.GenerateFilledArray(randomWeight[weight]))
 
 		}
-		// Calculate outer product of bias_gradient and input to layer to calculate weight gradient
-		gradients.WeightGradient = model.utils.Outer(&gradients.BiasGradient, input, model.OutputUnit, model.InputUnit, lr)
 
-		return gradients
+	}
 
-	} else {
+	return Dense{utils, inputUnit, outputUnit, weights, bias, activation}
 
-		var sum ckks.Ciphertext
+}
 
-		for i := range nextLayerTransposedWeight {
+func (d Dense) Forward(input []*ckks.Ciphertext) []*ckks.Ciphertext {
 
-			if i == 0 {
+	output := make([]*ckks.Ciphertext, d.OutputUnit)
 
-				// Calculate dot product and replace sum with new ciphertext
-				sum = model.utils.DotProductNew(nextLayerTransposedWeight[i], gradient, false)
+	for node := range d.Weights{
 
-				// Apply filter to dot product
-				model.utils.MultiplyPlain(&sum, &model.utils.Filters[i], &sum, true, false)
+		for datapoint := range d.Weights[node]{
+			
+			product := d.utils.MultiplyNew(*input[datapoint], *d.Weights[node][datapoint], true, false)
 
+			if output[node] == nil{
+				output[node] = &product
 			} else {
-
-				// Calculate dot product
-				product := model.utils.DotProductNew(nextLayerTransposedWeight[i], gradient, false)
-
-				// Apply filter to dot product
-				model.utils.MultiplyPlain(&product, &model.utils.Filters[i], &product, true, false)
-
-				// Add filtered product to sum ciphertext
-				model.utils.Add(product, sum, &sum)
-
+				d.utils.Add(*output[node], product, output[node])
 			}
 
 		}
 
-		// Apply activation gradient
-		if model.UseActivation {
-			activationGradient := model.Activation.Backward(*output, model.OutputUnit)
-			gradients.BiasGradient = model.utils.MultiplyNew(activationGradient, sum, true, false)
-		} else {
-			gradients.BiasGradient = sum
+		if d.Bias[node] != nil{
+			d.utils.Add(*output[node], *d.Bias[node], output[node])
 		}
 
-		// Calculate weight gradient by doing outer product
-		gradients.WeightGradient = model.utils.Outer(&gradients.BiasGradient, input, model.OutputUnit, model.InputUnit, lr)
-
-		return gradients
-
 	}
+
+	return output
 
 }
 
-func (d *Dense) UpdateGradient(gradient NeuralNetworkGradient) {
+// input is A(l-1) - activation of previous layer
+// output is Z(l) - output of this layer
+// gradient is ∂L/∂A(l) - influence that the activation of this layer has on the next layer
+// hasPrevLayer determine whether this function calculates the gradient ∂L/∂A(l-1)
+func (d *Dense) Backward(input []*ckks.Ciphertext, output []*ckks.Ciphertext, gradient []*ckks.Ciphertext, hasPrevLayer bool) DenseGradient{
 
-	d.utils.Sub(d.Bias, gradient.BiasGradient, &gradient.BiasGradient)
+	gradients := DenseGradient{}
 
-	for i := range d.Weights {
-		d.utils.Sub(d.Weights[i], gradient.WeightGradient[i], &d.Weights[i])
+	// Calculate gradients for last layer
+	if d.Activation != nil {
+		for b := range d.Bias{
+			activationGradient := (*d.Activation).Backward(*output[b], d.OutputUnit)
+			d.utils.Multiply(*gradient[b], activationGradient, gradient[b], true, false)
+		}
+	} 
+
+	gradients.BiasGradient = gradient
+	gradients.WeightGradient = d.utils.Outer2d(gradients.BiasGradient, input)
+	gradients.PrevLayerGradient = make([]*ckks.Ciphertext, d.InputUnit)
+	
+	if hasPrevLayer {
+
+		// Calculate ∂L/∂A(l-1)
+		transposedWeight := d.utils.Transpose2d(d.Weights)
+
+		for row := range transposedWeight{
+			
+			for datapoint := range gradients.BiasGradient{
+
+				grad := d.utils.MultiplyNew(*transposedWeight[row][datapoint], *gradients.BiasGradient[datapoint], true, false)
+
+				if gradients.PrevLayerGradient[row] == nil{
+					gradients.PrevLayerGradient[row] = &grad
+				} else {
+					d.utils.Add(*gradients.PrevLayerGradient[row], grad, gradients.PrevLayerGradient[row])
+				}
+
+			}
+		}
+
 	}
+
+	return gradients
 
 }
