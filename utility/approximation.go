a0f8228dffa8055a649c8ea6e65b4b26b53ff3b1
Pheem <sg.pheem@gmail.com>
2021-08-31 22:40:23 +0700

    change approx

--- utility/approximation.go
+++ utility/approximation.go
@@ -1,75 +1,108 @@
 package utility
 
 import (
-
 	"math"
 
 	"github.com/ldsec/lattigo/v2/ckks"
 )
 
 func (u Utils) ExpNew(ciphertext *ckks.Ciphertext) *ckks.Ciphertext {
+	//1 + x + x^2/2 + x^3/6 + x^4/24 + x^5/120 + x^6/720 + x^7/5040
+	// coeffs := []complex128{
+	// 	complex(1.0, 0),
+	// 	complex(1.0, 0),
+	// 	complex(1.0/2, 0),
+	// 	complex(1.0/6, 0),
+	// 	complex(1.0/24, 0),
+	// 	complex(1.0/120, 0),
+	// 	complex(1.0/720, 0),
+	// 	complex(1.0/5040, 0),
+	// }
 
-	coeffs := []complex128{
-		complex(1.0, 0),
-		complex(1.0, 0),
-		complex(1.0/2, 0),
-		complex(1.0/6, 0),
-		complex(1.0/24, 0),
-		complex(1.0/120, 0),
-		complex(1.0/720, 0),
-		complex(1.0/5040, 0),
-	}
+	//deg1
+	x := ciphertext
+	deg1 := x.CopyNew()
 
-	poly := ckks.NewPoly(coeffs)
+	//deg2
+	x2 := u.MultiplyNew(*x.CopyNew(), *x.CopyNew(), true, false)
+	deg2 := u.MultiplyConstNew(x2.CopyNew(), 0.5, true, false)
+	sum := u.AddNew(*deg1, deg2)
 
-	var err error
-	var result *ckks.Ciphertext
+	//deg3
+	x3 := u.MultiplyNew(*x.CopyNew(), *x2.CopyNew(), true, false)
+	deg3 := u.MultiplyConstNew(x3.CopyNew(), 1.0/6, true, false)
+	sum = u.AddNew(deg3, sum)
 
-	if result, err = u.Evaluator.EvaluatePoly(ciphertext, poly, ciphertext.Scale); err != nil {
-		panic(err)
-	}
+	//deg4
+	x4 := u.MultiplyNew(*x2.CopyNew(), *x2.CopyNew(), true, false)
+	deg4 := u.MultiplyConstNew(x4.CopyNew(), 1.0/24, true, false)
+	sum = u.AddNew(deg4, sum)
 
-	return result
+	//deg5
+	x5 := u.MultiplyNew(*x.CopyNew(), *x4.CopyNew(), true, false)
+	deg5 := u.MultiplyConstNew(x5.CopyNew(), 1.0/120, true, false)
+	sum = u.AddNew(deg5, sum)
+
+	//deg6
+	x6 := u.MultiplyNew(*x4.CopyNew(), *x2.CopyNew(), true, false)
+	deg6 := u.MultiplyConstNew(x6.CopyNew(), 1.0/720, true, false)
+	sum = u.AddNew(deg6, sum)
+
+	//deg7
+	x7 := u.MultiplyNew(*x4.CopyNew(), *x3.CopyNew(), true, false) //now deg6
+	deg7 := u.MultiplyConstNew(x7.CopyNew(), 1.0/5040, true, false)
+	sum = u.AddNew(deg7, sum)
+
+	//add 1
+	sum = *u.AddConstNew(&sum, u.GenerateFilledArray(1.0))
+
+	return &sum
 
 }
 
+// This function is used to calculate 1 / (n * stretchScale)
 func (u Utils) InverseApproxNew(ciphertext *ckks.Ciphertext, stretchScale float64) *ckks.Ciphertext {
-
-	// This function is used to calculate 1 / (n * stretchScale)
 
 	// Degree 7 approximation of inverse function
 
-	coeffs := []complex128{
-		complex(8, 0),
-		complex(-28, 0),
-		complex(56, 0),
-		complex(-70, 0),
-		complex(56, 0),
-		complex(-28, 0),
-		complex(8, 0),
-		complex(-1, 0),
-	}
+	//deg1
+	x := ciphertext
+	deg1 := u.MultiplyConstNew(x.CopyNew(), stretchScale*-28.0, true, false)
 
-	if stretchScale != 1 {
+	//deg2
+	x2 := u.MultiplyNew(*x.CopyNew(), *x.CopyNew(), true, false)
+	deg2 := u.MultiplyConstNew(x2.CopyNew(), math.Pow(stretchScale, 2)*56.0, true, false)
+	sum := u.AddNew(deg1, deg2)
 
-		for i, coeff := range coeffs {
+	//deg3
+	x3 := u.MultiplyNew(*x.CopyNew(), *x2.CopyNew(), true, false)
+	deg3 := u.MultiplyConstNew(x3.CopyNew(), math.Pow(stretchScale, 3)*-70.0, true, false)
+	sum = u.AddNew(deg3, sum)
 
-			coeffs[i] = complex(real(coeff)*math.Pow(stretchScale, float64(i)), 0)
+	//deg4
+	x4 := u.MultiplyNew(*x2.CopyNew(), *x2.CopyNew(), true, false)
+	deg4 := u.MultiplyConstNew(x4.CopyNew(), math.Pow(stretchScale, 4)*56.0, true, false)
+	sum = u.AddNew(deg4, sum)
 
-		}
+	//deg5
+	x5 := u.MultiplyNew(*x.CopyNew(), *x4.CopyNew(), true, false)
+	deg5 := u.MultiplyConstNew(x5.CopyNew(), math.Pow(stretchScale, 5)*-28.0, true, false)
+	sum = u.AddNew(deg5, sum)
 
-	}
+	//deg6
+	x6 := u.MultiplyNew(*x4.CopyNew(), *x2.CopyNew(), true, false)
+	deg6 := u.MultiplyConstNew(x6.CopyNew(), math.Pow(stretchScale, 6)*8.0, true, false)
+	sum = u.AddNew(deg6, sum)
 
-	poly := ckks.NewPoly(coeffs)
+	//deg7
+	x7 := u.MultiplyNew(*x4.CopyNew(), *x3.CopyNew(), true, false) //now deg6
+	deg7 := u.MultiplyConstNew(x7.CopyNew(), math.Pow(stretchScale, 7)*-1.0, true, false)
+	sum = u.AddNew(deg7, sum)
 
-	var err error
-	var result *ckks.Ciphertext
+	//add 1
+	sum = *u.AddConstNew(&sum, u.GenerateFilledArray(8.0))
 
-	if result, err = u.Evaluator.EvaluatePoly(ciphertext, poly, ciphertext.Scale); err != nil {
-		panic(err)
-	}
-
-	return result
+	return &sum
 
 }
 
@@ -94,7 +127,7 @@ func (u Utils) InverseNew(ct *ckks.Ciphertext, horizontalStretchScale float64) c
 
 }
 
-// Polynomial degree 3 approximation function of square root. Bound between [0, bound]. Set scaleBoundBack to true to get sqrt(ct) when bound > 1.75
+// Polynomial degree 3 approximation function of square root. Bound between [0, bound]. Set scaleBoundBack to true to get sqrt(ct) when bound > 1.75.
 // Please note that as bound increase the accuracy of approximation will decrease
 func (u Utils) SqrtApprox(ct *ckks.Ciphertext, degree int, bound float64, scaleBoundBack bool) *ckks.Ciphertext {
 
