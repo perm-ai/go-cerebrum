ebf91e0f8627a4aa862e10318c80597b285ae475
Pheem <sg.pheem@gmail.com>
2021-09-02 22:32:54 +0700

    Change activation to input array instead

--- activations/relu.go
+++ activations/relu.go
@@ -9,60 +9,66 @@ type Relu struct {
 	U utility.Utils
 }
 
-func (r Relu) Forward(input ckks.Ciphertext, inputLength int) ckks.Ciphertext {
+func (r Relu) Forward(input []*ckks.Ciphertext, inputLength int) []*ckks.Ciphertext {
 
 	// implement relu approximation according to equation (-1/120)x^4 + (5/24)x^2 + (1/2)x + 0.3
+	output := make([]*ckks.Ciphertext, len(input))
+	for i, inputEach := range input {
 
-	// calculate degree 4
-	xSquared := r.U.MultiplyNew(*input.CopyNew(), *input.CopyNew(), true, false)
-	xForthed := r.U.MultiplyNew(xSquared, xSquared, true, false)
-	deg4coeff := r.U.EncodePlaintextFromArray(r.U.GenerateFilledArraySize((-1.0 / 120.0), inputLength))
-	deg4 := r.U.MultiplyPlainNew(&xForthed, &deg4coeff, true, false)
+		// calculate degree 4
+		xSquared := r.U.MultiplyNew(*inputEach.CopyNew(), *inputEach.CopyNew(), true, false)
+		xForthed := r.U.MultiplyNew(xSquared, xSquared, true, false)
+		deg4coeff := r.U.EncodePlaintextFromArray(r.U.GenerateFilledArraySize((-1.0 / 120.0), inputLength))
+		deg4 := r.U.MultiplyPlainNew(&xForthed, &deg4coeff, true, false)
 
-	// calculate degree 2
-	deg2coeff := r.U.EncodePlaintextFromArray(r.U.GenerateFilledArraySize((5.0 / 24.0), inputLength))
-	deg2 := r.U.MultiplyPlainNew(&xSquared, &deg2coeff, true, false)
+		// calculate degree 2
+		deg2coeff := r.U.EncodePlaintextFromArray(r.U.GenerateFilledArraySize((5.0 / 24.0), inputLength))
+		deg2 := r.U.MultiplyPlainNew(&xSquared, &deg2coeff, true, false)
 
-	// calculate degree 1
-	deg1coeff := r.U.EncodePlaintextFromArray(r.U.GenerateFilledArraySize((0.5), inputLength))
-	deg1 := r.U.MultiplyPlainNew(input.CopyNew(), &deg1coeff, true, false)
+		// calculate degree 1
+		deg1coeff := r.U.EncodePlaintextFromArray(r.U.GenerateFilledArraySize((0.5), inputLength))
+		deg1 := r.U.MultiplyPlainNew(inputEach.CopyNew(), &deg1coeff, true, false)
 
-	// calculate degree 0
-	deg0coeff := r.U.EncodePlaintextFromArray(r.U.GenerateFilledArraySize((0.3), inputLength))
+		// calculate degree 0
+		deg0coeff := r.U.EncodePlaintextFromArray(r.U.GenerateFilledArraySize((0.3), inputLength))
 
-	// put everything together
-	result1 := r.U.AddNew(deg4, deg2)
-	result2 := r.U.AddNew(deg1, result1)
-	result3 := r.U.AddPlainNew(result2, deg0coeff)
-
-	return result3
+		// put everything together
+		result1 := r.U.AddNew(deg4, deg2)
+		result2 := r.U.AddNew(deg1, result1)
+		result3 := r.U.AddPlainNew(result2, deg0coeff)
+		output[i] = &result3
+	}
+	return output
 
 }
 
-func (r Relu) Backward(input ckks.Ciphertext, inputLength int) ckks.Ciphertext {
+func (r Relu) Backward(input []*ckks.Ciphertext, inputLength int) []*ckks.Ciphertext {
 	// (-1/30)x^3 + (10/24)x + 0.5
+	output := make([]*ckks.Ciphertext, len(input))
+	for i, inputEach := range input {
 
-	//calculate deg3
-	xSquared := r.U.MultiplyNew(*input.CopyNew(), *input.CopyNew(), true, false)
-	deg3coeff := r.U.EncodePlaintextFromArray(r.U.GenerateFilledArraySize((-4.0 / 120.0), inputLength))
-	xAndDeg3coeff := r.U.MultiplyPlainNew(input.CopyNew(), &deg3coeff, true, false)
-	deg3 := r.U.MultiplyNew(xSquared, xAndDeg3coeff, true, false)
+		//calculate deg3
+		xSquared := r.U.MultiplyNew(*inputEach.CopyNew(), *inputEach.CopyNew(), true, false)
+		deg3coeff := r.U.EncodePlaintextFromArray(r.U.GenerateFilledArraySize((-4.0 / 120.0), inputLength))
+		xAndDeg3coeff := r.U.MultiplyPlainNew(inputEach.CopyNew(), &deg3coeff, true, false)
+		deg3 := r.U.MultiplyNew(xSquared, xAndDeg3coeff, true, false)
 
-	//calculate deg1
+		//calculate deg1
 
-	deg1coeff := r.U.EncodePlaintextFromArray(r.U.GenerateFilledArraySize((10.0 / 24.0), inputLength))
-	deg1 := r.U.MultiplyPlainNew(input.CopyNew(), &deg1coeff, true, false)
+		deg1coeff := r.U.EncodePlaintextFromArray(r.U.GenerateFilledArraySize((10.0 / 24.0), inputLength))
+		deg1 := r.U.MultiplyPlainNew(inputEach.CopyNew(), &deg1coeff, true, false)
 
-	//calculate deg0
+		//calculate deg0
 
-	deg0coeff := r.U.EncodePlaintextFromArray(r.U.GenerateFilledArraySize((0.5), inputLength))
+		deg0coeff := r.U.EncodePlaintextFromArray(r.U.GenerateFilledArraySize((0.5), inputLength))
 
-	//add everything together
+		//add everything together
 
-	result1 := r.U.AddNew(deg3, deg1)
-	result2 := r.U.AddPlainNew(result1, deg0coeff)
-
-	return result2
+		result1 := r.U.AddNew(deg3, deg1)
+		result2 := r.U.AddPlainNew(result1, deg0coeff)
+		output[i] = &result2
+	}
+	return output
 }
 
 func (r Relu) GetForwardLevelConsumption() int {
--- activations/sigmoid.go
+++ activations/sigmoid.go
@@ -15,56 +15,62 @@ type Sigmoid struct {
 	backwardDeg0 map[int]ckks.Plaintext
 }
 
-func (s Sigmoid) Forward(input ckks.Ciphertext, inputLength int) ckks.Ciphertext {
+func (s Sigmoid) Forward(input []*ckks.Ciphertext, inputLength int) []*ckks.Ciphertext {
+	output := make([]*ckks.Ciphertext, len(input))
+	for i, inputEach := range input {
 
-	// y := 0.5 + 0.197x - 0.004x^3
-	// Calculate degree three
-	xSquared := s.U.MultiplyNew(*input.CopyNew(), *input.CopyNew(), true, false)
-	deg3Coeff := s.U.EncodePlaintextFromArray(s.U.GenerateFilledArraySize(-0.004, inputLength))
-	deg3 := s.U.MultiplyPlainNew(input.CopyNew(), &deg3Coeff, true, false)
-	s.U.Multiply(xSquared, deg3, &deg3, true, false)
+		// y := 0.5 + 0.197x - 0.004x^3
+		// Calculate degree three
+		xSquared := s.U.MultiplyNew(*inputEach.CopyNew(), *inputEach.CopyNew(), true, false)
+		deg3Coeff := s.U.EncodePlaintextFromArray(s.U.GenerateFilledArraySize(-0.004, inputLength))
+		deg3 := s.U.MultiplyPlainNew(inputEach.CopyNew(), &deg3Coeff, true, false)
+		s.U.Multiply(xSquared, deg3, &deg3, true, false)
 
-	// Calculate degree one
-	deg1Coeff := s.U.EncodePlaintextFromArray(s.U.GenerateFilledArraySize(0.197, inputLength))
-	deg1 := s.U.MultiplyPlainNew(input.CopyNew(), &deg1Coeff, true, false)
+		// Calculate degree one
+		deg1Coeff := s.U.EncodePlaintextFromArray(s.U.GenerateFilledArraySize(0.197, inputLength))
+		deg1 := s.U.MultiplyPlainNew(inputEach.CopyNew(), &deg1Coeff, true, false)
 
-	// Encode deg0 as plaintext
-	var deg0 ckks.Plaintext
-	if _, ok := s.forwardDeg0[inputLength]; ok {
-		deg0 = s.forwardDeg0[inputLength]
-	} else {
-		deg0 = *s.U.Encoder.EncodeNTTNew(s.U.Float64ToComplex128(s.U.GenerateFilledArraySize(0.5, inputLength)), s.U.Params.LogSlots())
+		// Encode deg0 as plaintext
+		var deg0 ckks.Plaintext
+		if _, ok := s.forwardDeg0[inputLength]; ok {
+			deg0 = s.forwardDeg0[inputLength]
+		} else {
+			deg0 = *s.U.Encoder.EncodeNTTNew(s.U.Float64ToComplex128(s.U.GenerateFilledArraySize(0.5, inputLength)), s.U.Params.LogSlots())
+		}
+
+		// Add all degree together
+		result := s.U.AddNew(deg3, deg1)
+		s.U.AddPlain(&result, &deg0, &result)
+		output[i] = &result
 	}
-
-	// Add all degree together
-	result := s.U.AddNew(deg3, deg1)
-	s.U.AddPlain(&result, &deg0, &result)
-	return result
+	return output
 
 }
 
-func (s Sigmoid) Backward(input ckks.Ciphertext, inputLength int) ckks.Ciphertext {
+func (s Sigmoid) Backward(input []*ckks.Ciphertext, inputLength int) []*ckks.Ciphertext {
+	output := make([]*ckks.Ciphertext, len(input))
+	for i, inputEach := range input {
+		// 0.012x^2 + 0.197
 
-	// 0.012x^2 + 0.197
+		// Calculate degree three
+		xSquared := s.U.MultiplyNew(*inputEach.CopyNew(), *inputEach.CopyNew(), true, false)
+		deg2Coeff := s.U.EncodePlaintextFromArray(s.U.GenerateFilledArraySize(0.012, inputLength))
+		deg2 := s.U.MultiplyPlainNew(&xSquared, &deg2Coeff, true, false)
 
-	// Calculate degree three
-	xSquared := s.U.MultiplyNew(*input.CopyNew(), *input.CopyNew(), true, false)
-	deg2Coeff := s.U.EncodePlaintextFromArray(s.U.GenerateFilledArraySize(0.012, inputLength))
-	deg2 := s.U.MultiplyPlainNew(&xSquared, &deg2Coeff, true, false)
+		// Encode deg0 as plaintext
+		var deg0 ckks.Plaintext
 
-	// Encode deg0 as plaintext
-	var deg0 ckks.Plaintext
+		if _, ok := s.backwardDeg0[inputLength]; ok {
+			deg0 = s.backwardDeg0[inputLength]
+		} else {
+			deg0 = *s.U.Encoder.EncodeNTTNew(s.U.Float64ToComplex128(s.U.GenerateFilledArraySize(0.197, inputLength)), s.U.Params.LogSlots())
+		}
 
-	if _, ok := s.backwardDeg0[inputLength]; ok {
-		deg0 = s.backwardDeg0[inputLength]
-	} else {
-		deg0 = *s.U.Encoder.EncodeNTTNew(s.U.Float64ToComplex128(s.U.GenerateFilledArraySize(0.197, inputLength)), s.U.Params.LogSlots())
+		// Add all degree together
+		result := s.U.AddPlainNew(deg2, deg0)
+		output[i] = &result
 	}
-
-	// Add all degree together
-	result := s.U.AddPlainNew(deg2, deg0)
-
-	return result
+	return output
 
 }
 
