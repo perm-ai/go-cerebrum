f647dd3e790a42eb524aa4369cc3df806139e12f
phanuruj <phuphanuruj@gmail.com>
2022-01-12 16:10:16 +0700

    Create meen_squared.go

---
+++ losses/meen_squared.go
@@ -0,0 +1,17 @@
+package losses
+
+import (
+	"github.com/perm-ai/go-cerebrum/utility"
+)
+
+type MSE struct {
+	U utility.Utils
+}
+
+func (m MSE) Forward() {
+
+}
+
+func (m MSE) Backward() {
+
+}
