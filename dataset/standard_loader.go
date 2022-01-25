94b1e1c939173d35d11c72f7723cb5d2f19b747a
FongChanatip <fongchanatip@gmail.com>
2022-01-25 21:34:17 +0700

    Add standard loader

---
+++ dataset/standard_loader.go
@@ -0,0 +1,41 @@
+package dataset
+
+import "github.com/ldsec/lattigo/v2/ckks"
+
+type StandardLoader struct{
+
+	X 	[]*ckks.Ciphertext
+	Y	[]*ckks.Ciphertext
+
+}
+
+func NewStandardLoader(dataX map[string]*ckks.Ciphertext, order []string, dataY []*ckks.Ciphertext) StandardLoader {
+
+	x := make([]*ckks.Ciphertext, len(order))
+
+	for i := range order{
+
+		x[i] = dataX[order[i]]
+
+	}
+
+	return StandardLoader{X: x, Y: dataY}
+
+}
+
+func (s StandardLoader) GetLength() int {
+	return len(s.X)
+}
+
+func (s StandardLoader) Load1D(start int, batchSize int) ([]*ckks.Ciphertext, []*ckks.Ciphertext){
+
+	return s.X[start: start+batchSize], s.Y[start: start+batchSize]
+
+}
+
+func (s StandardLoader) Load2D(start int, batchSize int) ([][][]*ckks.Ciphertext, []*ckks.Ciphertext){
+
+	// TODO: implement later
+	return [][][]*ckks.Ciphertext{}, []*ckks.Ciphertext{}
+
+}
