01ac92aabdfc3c4cfc28db558eeae64547111fed
phanuruj <phuphanuruj@gmail.com>
2022-01-27 13:34:35 +0700

    create neural network model

--- main.go
+++ main.go
@@ -3,16 +3,41 @@ package main
 import (
 	"fmt"
 	"math"
+	"os"
 
+	"github.com/ldsec/lattigo/v2/ckks"
 	"github.com/perm-ai/go-cerebrum/activations"
+	"github.com/perm-ai/go-cerebrum/dataset"
 	"github.com/perm-ai/go-cerebrum/key"
 	"github.com/perm-ai/go-cerebrum/layers"
+	"github.com/perm-ai/go-cerebrum/logger"
 	"github.com/perm-ai/go-cerebrum/losses"
+	"github.com/perm-ai/go-cerebrum/management"
 	"github.com/perm-ai/go-cerebrum/models"
 	"github.com/perm-ai/go-cerebrum/utility"
 )
 
 func main() {
+
+	BATCH_SIZE := 40
+	LEARNING_RATE := 0.3
+	EPOCH := 100
+
+	importedData, _ := management.LoadJsonData("/usr/local/go/src/github.com/perm-ai/go-cerebrum/importer/test-data/mnist_handwritten_train.json")
+
+	xCipherTexts := make(map[string]*ckks.Ciphertext)
+	yCipherTexts := make([]*ckks.Ciphertext, 1)
+
+	for i := range importedData.Encrypted {
+		if importedData.Encrypted[i].ColumnName != "price" {
+			xCipherTexts[importedData.Encrypted[i].ColumnName] = importedData.Encrypted[i].Data
+		} else {
+			yCipherTexts[0] = importedData.Encrypted[i].Data
+		}
+	}
+
+	order := []string{"TM_AR", "TS_AR", "M_AD", "ASH_AD", "ASH_AR", "Sulfate_SO3", "Silica_SiO2", "Calcium_CaO", "Iron_Fe2O3"}
+
 	keyPair := key.LoadKeys("/usr/local/go/src/github.com/perm-ai/go-cerebrum/keychain", 0, true, true, false, false)
 	keychain := key.GenerateKeysFromKeyPair(0, keyPair.SecretKey, keyPair.PublicKey, true, true)
 
@@ -34,4 +59,15 @@ func main() {
 
 	model := models.NewModel(utils, []layers.Layer1D{&dense1, &dense2}, []layers.Layer2D{}, losses.MSE{}, true)
 
+	loader := dataset.NewStandardLoader(xCipherTexts, order, yCipherTexts)
+
+	timer := logger.StartTimer("Neural Network Training")
+
+	model.Train1D(loader, LEARNING_RATE, BATCH_SIZE, EPOCH)
+
+	timer.LogTimeTakenSecond()
+
+	os.Mkdir("test_model_banpu_1", 0777)
+
+	model.ExportModel1D("test_model_banpu_1")
 }
