3cc6b3489f563c955704d730803d8ed46487d744
phanuruj <phuphanuruj@gmail.com>
2022-02-03 21:33:49 +0700

    Revert "Update dense.go"
    This reverts commit ce8e67f41e2c8d34c45c55a40edfa74b45aa394f.

--- layers/dense.go
+++ layers/dense.go
@@ -412,7 +412,7 @@ func (d *Dense) UpdateGradient(gradient Gradient1d, lr float64) {
 
 						// Drop level to minimum requirement + 1 for faster evaluation
 						if gradient.WeightGradient[nodeIndex][weightIndex].Level() > d.weightLevel+1 {
-							weightUtils.Evaluator.DropLevel(gradient.WeightGradient[nodeIndex][weightIndex], gradient.WeightGradient[nodeIndex][weightIndex].Level()-(d.weightLevel))
+							weightUtils.Evaluator.DropLevel(gradient.WeightGradient[nodeIndex][weightIndex], gradient.WeightGradient[nodeIndex][weightIndex].Level()-(d.weightLevel+1))
 						}
 
 						// Perform Ciphertext inner sum and average
